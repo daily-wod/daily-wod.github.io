d8cdfc659d4d1f260d30f8bb3a50763935e2c6f3
noah <noah.lee@buzzvil.com>
2022-11-21 10:10:37 +0900

    Enable google analytics

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -49,6 +49,9 @@ const config = {
         theme: {
           customCss: require.resolve("./src/css/custom.css"),
         },
+        gtag: {
+          trackingID: "G-EZQ5GRGJR3",
+        },
       }),
     ],
   ],
